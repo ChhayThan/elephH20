c3c6462ae576e30581e4c46da95ac5389d21dc9a
Arvin-Leung <leungarvin@gmail.com>
2023-11-04 15:41:18 -0400

    Added Scroll Effect to landing page

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
 var theme = document.getElementById("theme");
 
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
+
 theme.onclick = function() {
     document.body.classList.toggle("darktheme");
     if(document.body.classList.contains("darktheme")) {
